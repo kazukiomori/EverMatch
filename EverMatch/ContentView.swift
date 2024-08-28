dd8ce0d3b89ab40980a4d3e8812c431470fdaf4b
kazukiomori <kazuki1110170501@gmail.com>
2024-08-29 01:38:52 +0900

    Initial Commit

---
+++ EverMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EverMatch
+//
+//  Created by Kazuki Omori on 2024/08/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EverMatch/EverMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverMatchApp.swift
+//  EverMatch
+//
+//  Created by Kazuki Omori on 2024/08/29.
+//
+
+import SwiftUI
+
+@main
+struct EverMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
