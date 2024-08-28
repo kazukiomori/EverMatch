2ab329406ca615a48acccbb23746450a7395166b
kazukiomori <kazuki1110170501@gmail.com>
2024-08-29 03:02:40 +0900

    [add]リスト画面の大まかなレイアウトを作成

--- EverMatch/View/ListView.swift
+++ EverMatch/View/ListView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            // cards
+            ZStack {
+                ForEach(0..<5) { _ in
+                    Color.gray
+                }
+            }
+            
+            //action
+            HStack(spacing: 68) {
+                Circle()
+                    .frame(width: 50, height: 50)
+                Circle()
+                    .frame(width: 50, height: 50)
+                Circle()
+                    .frame(width: 50, height: 50)
+            }
+            .foregroundStyle(.white)
+            .frame(height: 100)
+        }
+        .background(.black, in: RoundedRectangle(cornerRadius: 15))
+        .padding(.horizontal, 6)
     }
 }
 
