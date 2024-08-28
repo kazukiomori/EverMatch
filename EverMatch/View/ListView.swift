c5d1c07b62d8a8a3cbe4c01b080b342b8870ae61
kazukiomori <kazuki1110170501@gmail.com>
2024-08-29 03:19:50 +0900

    [add]カード部分の大まかなレイアウトを作成

--- EverMatch/View/ListView.swift
+++ EverMatch/View/ListView.swift
@@ -13,7 +13,39 @@ struct ListView: View {
             // cards
             ZStack {
                 ForEach(0..<5) { _ in
-                    Color.gray
+                    ZStack(alignment: .bottom) {
+                        // background
+                        Color.black
+                        
+                        // image
+                        Image("avatar")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100)
+                        
+                        // gradient
+                        LinearGradient(colors: [.clear, .black], startPoint: .center, endPoint: .bottom)
+                        
+                        // information
+                        VStack(alignment: .leading) {
+                            HStack(alignment: .bottom) {
+                                Text("ブルー")
+                                    .font(.largeTitle.bold())
+                                
+                                Text("99")
+                                    .font(.title2)
+                                
+                                Circle()
+                                    .frame(width: 22, height: 22)
+                            }
+                            Text("よろしくお願いします。")
+                                
+                        }
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
                 }
             }
             
