2f9b54379d872dab94917ca9b4bd6fd950c06fc1
kazukiomori <kazuki1110170501@gmail.com>
2024-08-29 02:51:00 +0900

    [add]ListViewを生成

---
+++ EverMatch/View/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  EverMatch
+//
+//  Created by Kazuki Omori on 2024/08/29.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
