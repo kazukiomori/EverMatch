f728ee07a99f96c718fbce2e88fc61046833b9af
kazukiomori <kazuki1110170501@gmail.com>
2024-08-29 03:46:48 +0900

    [add]アクションエリアのUIを作成

--- EverMatch/View/ListView.swift
+++ EverMatch/View/ListView.swift
@@ -36,12 +36,44 @@ extension ListView {
     
     private var actions: some View {
         HStack(spacing: 68) {
-            Circle()
-                .frame(width: 50, height: 50)
-            Circle()
-                .frame(width: 50, height: 50)
-            Circle()
-                .frame(width: 50, height: 50)
+            Button(action: {
+                print("ボタンがタップされました")
+            }, label: {
+                Image(systemName: "xmark")
+                    .font(.system(size: 26, weight: .bold))
+                    .foregroundColor(.red)
+                    .background{
+                        Circle()
+                            .stroke(.red, lineWidth: 1)
+                            .frame(width: 60, height: 60)
+                    }
+            })
+            
+            Button(action: {
+                print("ボタンがタップされました")
+            }, label: {
+                Image(systemName: "arrow.counterclockwise")
+                    .font(.system(size: 26, weight: .bold))
+                    .foregroundColor(.yellow)
+                    .background{
+                        Circle()
+                            .stroke(.yellow, lineWidth: 1)
+                            .frame(width: 50, height: 50)
+                    }
+            })
+            
+            Button(action: {
+                print("ボタンがタップされました")
+            }, label: {
+                Image(systemName: "heart")
+                    .font(.system(size: 26, weight: .bold))
+                    .foregroundColor(.mint)
+                    .background{
+                        Circle()
+                            .stroke(.mint, lineWidth: 1)
+                            .frame(width: 60, height: 60)
+                    }
+            })
         }
         .foregroundStyle(.white)
         .frame(height: 100)
