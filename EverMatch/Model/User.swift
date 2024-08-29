1d59067658785ed57ae72297776f39efcb3d1e81
kazukiomori <kazuki1110170501@gmail.com>
2024-08-30 04:25:56 +0900

    [add]Userモデルの作成

---
+++ EverMatch/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  EverMatch
+//
+//  Created by Kazuki Omori on 2024/08/30.
+//
+
+import Foundation
+
+struct User {
+    let id: String
+    let name: String
+    let email: String
+    let age: Int
+    var photoUrl: String?
+    var message: String?
+}
