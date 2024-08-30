50d73a0fc3d004dfaf9f2a2e2e0c03b9029a843b
kazukiomori <kazuki1110170501@gmail.com>
2024-08-31 03:14:07 +0900

    [add]アクションボタンのコードを共通化

--- EverMatch/Utils/Action.swift
+++ EverMatch/Utils/Action.swift
@@ -5,10 +5,58 @@
 //  Created by Kazuki Omori on 2024/08/31.
 //
 
-import Foundation
+import SwiftUI
 
-enum Action {
+enum Action: CaseIterable {
     case nope
     case redo
     case like
+}
+
+extension Action {
+    func createActionButton(viewModel: ListViewModel) -> some View {
+        Button(action: {
+            viewModel.tappedHandler(action: self)
+        }, label: {
+            Image(systemName: self.symbol)
+                .font(.system(size: 26, weight: .bold))
+                .foregroundColor(self.color)
+                .background{
+                    Circle()
+                        .stroke(self.color, lineWidth: 1)
+                        .frame(width: self.size, height: self.size)
+                }
+        })
+    }
+    
+    private var symbol: String {
+        switch self {
+        case .nope:
+            return "xmark"
+        case .redo:
+            return "arrow.counterclockwise"
+        case .like:
+            return "heart"
+        }
+    }
+    
+    private var color: Color {
+        switch self {
+        case .nope:
+            return .red
+        case .redo:
+            return .yellow
+        case .like:
+            return .green
+        }
+    }
+    
+    private var size: CGFloat {
+        switch self {
+        case .nope, .like:
+            return 60
+        case .redo:
+            return 50
+        }
+    }
 }
--- EverMatch/View/ListView.swift
+++ EverMatch/View/ListView.swift
@@ -39,44 +39,9 @@ extension ListView {
     
     private var actions: some View {
         HStack(spacing: 68) {
-            Button(action: {
-                viewModel.tappedHandler(action: .nope)
-            }, label: {
-                Image(systemName: "xmark")
-                    .font(.system(size: 26, weight: .bold))
-                    .foregroundColor(.red)
-                    .background{
-                        Circle()
-                            .stroke(.red, lineWidth: 1)
-                            .frame(width: 60, height: 60)
-                    }
-            })
-            
-            Button(action: {
-                viewModel.tappedHandler(action: .redo)
-            }, label: {
-                Image(systemName: "arrow.counterclockwise")
-                    .font(.system(size: 26, weight: .bold))
-                    .foregroundColor(.yellow)
-                    .background{
-                        Circle()
-                            .stroke(.yellow, lineWidth: 1)
-                            .frame(width: 50, height: 50)
-                    }
-            })
-            
-            Button(action: {
-                viewModel.tappedHandler(action: .like)
-            }, label: {
-                Image(systemName: "heart")
-                    .font(.system(size: 26, weight: .bold))
-                    .foregroundColor(.mint)
-                    .background{
-                        Circle()
-                            .stroke(.mint, lineWidth: 1)
-                            .frame(width: 60, height: 60)
-                    }
-            })
+            ForEach(Action.allCases, id: \.self) { type in
+                type.createActionButton(viewModel: viewModel)
+            }
         }
         .foregroundStyle(.white)
         .frame(height: 100)
