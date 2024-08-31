69753bdccbd2b9066b1a1e56b066888afcae4bd5
kazukiomori <kazuki1110170501@gmail.com>
2024-09-01 05:23:28 +0900

    [add]ログイン画面のUIを作成

---
+++ EverMatch/View/Auth/LoginView.swift
@@ -0,0 +1,82 @@
+//
+//  LoginView.swift
+//  EverMatch
+//
+//  Created by Kazuki Omori on 2024/09/01.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    var body: some View {
+        VStack {
+            // image
+            Image(systemName: "flame.circle.fill")
+                .resizable()
+                .scaledToFill()
+                .foregroundStyle(.red)
+                .frame(width: 120, height: 120)
+                .padding(.vertical, 32)
+            
+            // Form
+            VStack(spacing: 24) {
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("メールアドレス")
+                        .foregroundStyle(Color(.darkGray))
+                        .fontWeight(.semibold)
+                        .font(.footnote)
+                    TextField("入力してください", text: $email)
+                    Divider()
+                }
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("パスワード")
+                        .foregroundStyle(Color(.darkGray))
+                        .fontWeight(.semibold)
+                        .font(.footnote)
+                    SecureField("半角英数字6文字以上", text: $password)
+                    Divider()
+                }
+                
+            }
+            
+            // Button
+            Button {
+                
+            } label: {
+                HStack {
+                    Text("ログイン")
+                    Image(systemName: "arrow.right")
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 16)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .background(.red)
+                .clipShape(Capsule())
+            }
+            .padding(.top, 24)
+            
+            Spacer()
+            
+            // Navigation
+            NavigationLink {
+                
+            } label: {
+                HStack {
+                    Text("まだアカウントをお持ちでない方")
+                    Text("会員登録")
+                        .fontWeight(.bold)
+                }
+                .foregroundStyle(Color(.darkGray))
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    LoginView()
+}
